253cff6f4d6889918599ebb97ff061667a85741d
Alexander Bolkhovityanov <alex.daiv@gmail.com>
2008-09-24 17:38:10 +0400

    First commit

---
+++ ZCSC/ZCSC/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZCSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
